23c98e79bbc1c20dd90ed089002b3bada93b01a8
Pervez <pervezhossain1998@gmail.com>
2022-11-23 20:42:58 +0600

    MiddleWare Setup Done

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const expres = require("express")
 const app = expres()
 const port = process.env.PORT || 5000
 
+// Middle Ware Setup
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+require("dotenv").config()
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Server Is Running")
