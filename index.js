31c11212aed5946f082e5445da93874137f3c041
Pervez <pervezhossain1998@gmail.com>
2022-11-24 01:01:38 +0600

    Server Setup Done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // Middle Ware Setup
 const cors = require("cors")
@@ -9,6 +11,22 @@ app.use(cors())
 app.use(express.json())
 require("dotenv").config()
 
+
+// Database Setup 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xu0oole.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+            
+    }
+    finally {
+        
+    }
+}
+run().catch(err=> console.log(error))
 
 
 
