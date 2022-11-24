9038d19b122368267b2b4fda1bb3bd7289a28f27
Pervez <pervezhossain1998@gmail.com>
2022-11-24 16:28:10 +0600

    Api Created For Getting All The User

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // Middle Ware Setup
@@ -32,6 +32,39 @@ async function run() {
             res.send(result)
         })
 
+        // Getting Products All Products
+        // app.get("/categories/:categoryId", async (req, res) => {
+        //     const categoryId = req.params.categoryId
+        //     console.log("This is", categoryId)
+           
+        //    const query = {categoryId : categoryId}
+        //     const result = await productCollection.find(query).toArray()
+        //     res.send(result)
+        // })
+
+
+        // Posting Product
+        app.post("/dashboard/addproducts", async (req, res) => {
+            const products = req.body
+            console.log(products)
+            const result = await productCollection.insertOne(products)
+            res.send(result)
+        })
+       
+
+        // Getting Seller From Database
+        app.get("/seller", async (req, res) => {
+            const query = {isSeller : true}
+            const result = await userCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Getting All Users From Database
+        app.get("/users", async (req, res) => {
+            const query = {}
+            const result = await userCollection.find(query).toArray()
+            res.send(result)
+        })
 
         // Store Users in Database
         app.post("/users", async (req, res) => {
@@ -44,7 +77,7 @@ async function run() {
         
     }
 }
-run().catch(err=> console.log(error))
+run().catch(err=> console.log(err))
 
 
 
